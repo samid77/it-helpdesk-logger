5dc9866733eb9783bfad8bf411992d5952f2419e
Samid77 <dimaseptyanto@windowslive.com>
2020-05-25 14:40:29 +0700

    Add log functionality and enhancing Redux

---
+++ src/actions/logActions.js
@@ -0,0 +1,56 @@
+import { 
+    GET_LOGS, 
+    SET_LOADING, 
+    LOGS_ERROR,
+    ADD_LOG
+} from './types'
+
+
+export const getLogs = () => async dispatch => {
+    try {
+        setLoading();
+        const res = await fetch('/logs');
+        const data = await res.json();
+
+        dispatch({
+            type: GET_LOGS,
+            payload: data
+        })   
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+}
+
+export const setLoading = () => {
+    return {
+        type: SET_LOADING
+    }
+}
+
+export const addLog = log => async dispatch => {
+    try {
+        setLoading();
+        const res = await fetch('/logs', {
+            method: 'POST',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.statusText
+        })
+    }
+}
