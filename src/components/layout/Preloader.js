4186c57b0c12d08cc6b7296232c3a46a7844d725
Samid77 <dimaseptyanto@windowslive.com>
2020-05-25 10:08:59 +0700

    Setup materialize, JSON server, add some components

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <div className='progress cyan lighten-4'>
+            <div className='indeterminate cyan'></div>
+        </div>
+    )
+}
+
+export default Preloader
